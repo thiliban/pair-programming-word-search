8267cc0b6fae40a757021683e831e14e151f727b
Thiliban Baskaran <thiliban.b@gmail.com>
2023-07-26 01:40:09 +0000

    Word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;    
     }
+    if (letters = []) {
+        return false;
+    }
+    for (let i = 0; i < word.length; i++){
+        if(letters[i].indexOf(word[i].join('')) > -1){
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
+
+
